cfe00f46df50149d16af81e0e9da77d626eb96c8
chamorroam <chamorroam@uninorte.edu.co>
2021-11-30 22:24:22 -0500

    ultimas mod

--- static/js/graphics.js
+++ static/js/graphics.js
@@ -23,18 +23,6 @@ if (date1 != '' && date2 != ''){
         }
     };
     hist()
-    // if (resultInput) {
-    //     resultInput = JSON.parse(resultInput);
-    //     var datas = [];
-    //     var dates = [];
-    //     for (var i = 0; i < resultInput.length; i++) {
-    //         datas[i] = resultInput[i]['data'];
-    //         dates[i] = resultInput[i]['date'];
-    //     }
-    //     for (i = 0; i < dates.length; i++) {
-    //         dates[i] = dates[i].split("T").join(" "); 
-    //     }
-    // }
 }
 
 
